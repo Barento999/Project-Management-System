165ea491f4604175134b9162585687dea6b15799
Barento999 <barentohashum11@gmail.com>
2026-01-07 16:53:44 +0300

    deploy

--- server/server.js
+++ server/server.js
@@ -26,7 +26,13 @@ const { errorHandler } = require("./middleware/errorHandler");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://project-management-system-b.vercel.app/"],
+    credentials: true,
+  })
+);
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
